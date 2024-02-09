94a99c64e6e44eeafc8f1ddc882752e77ea54662
Schmidt Nicolás <nicolasschmidt@asd.com>
2024-02-09 20:40:44 -0300

    add users page

---
+++ src/app/(header)/admin/users/page.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import styles from '@styles/table.module.scss';
+import { getUsers } from '@services/users/users.service';
+import { IPaginationResponse } from '@interfaces/pagination.interfaces';
+import { IUsers } from '@interfaces/users.interfaces';
+import { formatDateString } from '@utils/formaters/dateFormaters';
+
+const fetchData = async (
+  page: number
+): Promise<IPaginationResponse<IUsers>> => {
+  const data = await getUsers({ page });
+
+  return data;
+};
+
+const Users = async () => {
+  const users = await fetchData(1);
+
+  return (
+    <div>
+      <h4 className={styles.title}>Usuarios</h4>
+      <div className={styles.simpleTable}>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Email</th>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Slack</th>
+              <th>Supabase</th>
+              <th>Register</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.username}</td>
+                <td>{`${item.lastName} ${item.name}`}</td>
+                <td>{item.slackId}</td>
+                <td className={styles.w10}>{item.supabaseId}</td>
+                <td>{formatDateString(item.createdAt)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
