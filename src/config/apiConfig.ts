6b3ff80842e74002b9408022668890a836b13d88
Schmidt Nicolás <nicolasschmidt@asd.com>
2024-03-11 22:35:41 -0300

    fix base url env

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: process.env.BASE_URL ?? 'http://localhost:4000',
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:4000',
 };
 
 export default config;
