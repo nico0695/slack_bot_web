7657ef6c71fba123e014816f045486713e7322df
Schmidt Nicolás <nicolasschmidt@asd.com>
2024-04-08 17:55:08 -0300

    fix socket conection

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,8 @@
 const config = {
   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:4000',
+
+  SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL ?? 'http://localhost:4000',
+  SOCKET_PATH: process.env.NEXT_PUBLIC_SOCKET_PATH ?? '/socket.io',
 };
 
 export default config;
--- src/shared/utils/api/socket.ts
+++ src/shared/utils/api/socket.ts
@@ -1,6 +1,7 @@
 import apiConfig from '@config/apiConfig';
 import { io } from 'socket.io-client';
 
-export const socket = io(apiConfig.BASE_URL, {
-  transports: ['websocket'],
+export const socket = io(apiConfig.SOCKET_URL, {
+  path: apiConfig.SOCKET_PATH,
+  transports: ['websocket', 'polling'],
 });
